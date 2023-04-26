fdccd16c5466cb3f389cc66f19969e5a4847c0a2
TheanhNguyen <theanhnguyen177@gmail.com>
2023-04-26 11:18:08 +0700

    add 2D game

--- Background/index.js
+++ Background/index.js
@@ -1,14 +1,11 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-
-
-
 const WIDTH = (canvas.width = 800);
 const HEIGHT = (canvas.height = 700);
 
 let gameSpeed = 5;
-// let gameFrame = 0; 
+// let gameFrame = 0;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = "layer-1.png";
@@ -21,64 +18,64 @@ backgroundLayer4.src = "layer-4.png";
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = "layer-5.png";
 
-
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
     const slider = document.getElementById("slider");
-const showGameSpeed = document.getElementById("showGameSpeed");
-showGameSpeed.innerHTML = gameSpeed;
+    const showGameSpeed = document.getElementById("showGameSpeed");
+    showGameSpeed.innerHTML = gameSpeed;
 
-slider.addEventListener('change' , (e) => {
-    gameSpeed = e.target.value;
-    showGameSpeed.innerHTML = e.target.value
-})
+    slider.addEventListener("change", (e) => {
+        gameSpeed = e.target.value;
+        showGameSpeed.innerHTML = e.target.value;
+    });
 
-
-class Layer {
-    constructor(image, speedModifier) {
-        this.x = 0;
-        this.y = 0;
-        this.width = 2400;
-        this.height = 700;
-        this.x2 = this.width;
-        this.image = image;
-        this.speedModifier = speedModifier;
-        this.speed = gameSpeed * this.speedModifier;
-    }
-    update() {
-        this.speed = gameSpeed * this.speedModifier;
-        if (this.x <= -this.width) {
+    class Layer {
+        constructor(image, speedModifier) {
             this.x = 0;
-        
-        
+            this.y = 0;
+            this.width = 2400;
+            this.height = 700;
+            this.x2 = this.width;
+            this.image = image;
+            this.speedModifier = speedModifier;
+            this.speed = gameSpeed * this.speedModifier;
         }
-        this.x = this.x - this.speed;
-        // this.x = gameFrame* this.speed % this.width;
+        update() {
+            this.speed = gameSpeed * this.speedModifier;
+            if (this.x <= -this.width) {
+                this.x = 0;
+            }
+            this.x = this.x - this.speed;
+            // this.x = gameFrame* this.speed % this.width;
+        }
+        draw() {
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+            ctx.drawImage(
+                this.image,
+                this.x + this.width,
+                this.y,
+                this.width,
+                this.height
+            );
+        }
     }
-    draw() {
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+
+    const layer1 = new Layer(backgroundLayer1, 0.2);
+    const layer2 = new Layer(backgroundLayer2, 0.4);
+    const layer3 = new Layer(backgroundLayer3, 0.6);
+    const layer4 = new Layer(backgroundLayer4, 0.8);
+    const layer5 = new Layer(backgroundLayer5, 1);
+
+    const gameObject = [layer1, layer2, layer3, layer4, layer5];
+
+    function animate() {
+        ctx.clearRect(0, 0, WIDTH, HEIGHT);
+        gameObject.forEach((object) => {
+            object.update();
+            object.draw();
+        });
+        // gameFrame--;
+        requestAnimationFrame(animate);
     }
-}
 
-const layer1 = new Layer(backgroundLayer1, 0.2);
-const layer2 = new Layer(backgroundLayer2, 0.4);
-const layer3 = new Layer(backgroundLayer3, 0.6);
-const layer4 = new Layer(backgroundLayer4, 0.8);
-const layer5 = new Layer(backgroundLayer5, 1);
-
-const gameObject = [layer1, layer2, layer3, layer4, layer5];
-
-function animate() {
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
-    gameObject.forEach(object => {
-        object.update();
-        object.draw();
-    })
-    // gameFrame--;
-    requestAnimationFrame(animate);
-}
-
-animate();
-
-})
-
+    animate();
+});
